db75f7d007bc919a24d8e88ea905eca3dbd89057
yaminhameed <yaminhamid@outlook.com>
2020-12-05 15:16:08 +0530

    styled menu items

---
+++ src/Components/menu-items/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menu-item.style.scss'
+const MenuItem =({title, imageUrl,size}) => (
+    <div className={`menu-item ${size}`}>
+     <div className='background-image'   
+      style={{backgroundImage:`url(${imageUrl})`}} />
+        <div className='content'>
+            <h1 className='title'> {title.toUpperCase()} </h1>
+            <span classname='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+
+export default MenuItem;
